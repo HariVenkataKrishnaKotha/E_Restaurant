1fb93b03c55871d221408b4f9bfd42902929ed83
hvkkotha <hk00016@mix.wvu.edu>
2023-08-27 01:12:41 -0400

    Added CRUD functionality to the Coupon API Service

--- Erestaurant.Services.CouponAPI/Controllers/CouponAPI.cs
+++ Erestaurant.Services.CouponAPI/Controllers/CouponAPI.cs
@@ -4,6 +4,8 @@ using Erestaurant.Services.CouponAPI.Models;
 using Erestaurant.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Erestaurant.Services.CouponAPI.Controllers
 {
@@ -45,6 +47,73 @@ namespace Erestaurant.Services.CouponAPI.Controllers
                 Coupon obj = _db.Coupons.First(x => x.CouponId == id); ;
                 _response.Result = _mapper.Map<CouponDto>(obj);
             }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+
+
+                //To let EF Core handle the Identity Column
+                using var transaction = _db.Database.BeginTransaction();
+                _db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Coupons ON");
+
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+
+                // Disable IDENTITY_INSERT for Coupons table
+                _db.Database.ExecuteSqlRaw("SET IDENTITY_INSERT Coupons OFF");
+
+                transaction.Commit();
+
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+            }
+
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete([FromBody] int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(x => x.CouponId == id);
+                _db.Coupons.Remove(obj);
+                _db.SaveChanges();
+            }
+
             catch (Exception ex)
             {
                 _response.IsSuccess = false;
--- Erestaurant.Services.CouponAPI/Models/Dto/CouponDto.cs
+++ Erestaurant.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -4,12 +4,9 @@ namespace Erestaurant.Services.CouponAPI.Models.Dto
 {
     public class CouponDto
     {
-        public class Coupon
-        {
             public int CouponId { get; set; }
             public string CouponCode { get; set; }
             public double DiscountAmount { get; set; }
             public int MinAmount { get; set; }
-        }
     }
 }
